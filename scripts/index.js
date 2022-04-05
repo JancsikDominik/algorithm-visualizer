5473b6829d4c84dd4075f954a777e185d092b4c4
Jancsik Dominik <uaa156@inf.elte.hu>
2022-04-05 19:58:24 +0200

    implemented shuffle array button functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,24 @@
 const visualizationDiv = document.querySelector('div#visualizer');
 const arrSizeSlider = document.querySelector('input#array-size-slider');
 const startBtn = document.querySelector('button#start-btn');
-const makeArrbtn = document.querySelector('button#shuffle-array-btn');
+const shuffleArrbtn = document.querySelector('button#shuffle-array-btn');
 const algorithmSelect = document.querySelector('select#select-algorithm');
 
 const maxValue = 300;
 let arr = []
 
-// function that runs every time the site loads
-function init() {
+// ****************************************************
+//                    FUNCTIONS
+// ****************************************************
+
+// Initializes the array, and deletes the bars set by our previous init
+function initArr() {
+    // deleting array elements
+    arr = [];
+
+    // It's ok to use innerHTML here, because we set it to a constant defined by us.
+    // We overwrite the innerHTML in order to erase every child element
+    visualizationDiv.innerHTML = "";
 
     for (let i = 0; i < arrSizeSlider.value; i++) {
         // not using innerHTML, so that the site is safe against injections
@@ -21,22 +31,28 @@ function init() {
     }
 }
 
+// function that runs every time the site loads
+function initSite() {
+
+    initArr();
+}
+
+// ****************************************************
+//                      EVENTS
+// ****************************************************
+
 arrSizeSlider.addEventListener('input', e => {
 
-    arr = [];
-    // it's ok to use innerHTML here, because we set it to a constant defined by us
-    visualizationDiv.innerHTML = "";
-
-    for (let i = 0; i < arrSizeSlider.value; i++) {
-        // not using innerHTML, so that the site is safe against injections
-        let div = document.createElement('div');
-        div.setAttribute('class', 'bar');
-        // number between 1 and maxValue
-        arr.push(parseInt(Math.random() * (maxValue + 2) + 1));
-        div.style.height = arr[i] + "px";
-        visualizationDiv.appendChild(div);
-    }
+    initArr();
 
 });
 
-init();
+// Made this button, so that a user can try the same algorithm, without changing the size
+shuffleArrbtn.addEventListener('click', e => {
+
+    visualizationDiv.innerHTML = "";
+    initArr();
+
+});
+
+initSite();
