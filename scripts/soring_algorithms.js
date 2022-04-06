39df456482084f0fc9a3bec1be59771bb26fbb80
Jancsik Dominik <uaa156@inf.elte.hu>
2022-04-06 08:37:21 +0200

    implemented bubble sort

---
+++ scripts/soring_algorithms.js
@@ -0,0 +1,13 @@
+function bubbleSort(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+    return arr;
+}
