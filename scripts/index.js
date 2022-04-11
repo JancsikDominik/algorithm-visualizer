ae364ba9a0a50072a2b75e488427bea29679aded
Jancsik Dominik <uaa156@inf.elte.hu>
2022-04-11 12:53:23 +0200

    clicking on cards no redirects to the correct page

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+const cardsContainer = document.querySelector('div.cards-container');
+
+cardsContainer.addEventListener('click', e => {
+    if (e.target.matches('img')) {
+
+        const parent = e.target.parentElement;
+        const titleDiv = parent.getElementsByClassName('card-title');
+        const titleChildren = titleDiv[0].childNodes;
+        const a = titleChildren[1].firstChild;
+        let desiredLink = a.href;
+        console.log(desiredLink);
+
+        window.location.replace(desiredLink);
+    }
+    else if (e.target.matches('div.card-title')) {
+        const titleChildren = e.target.childNodes;
+        const a = titleChildren[1].firstChild;
+        let desiredLink = a.href;
+        console.log(desiredLink);
+
+        window.location.replace(desiredLink);
+    }
+});
